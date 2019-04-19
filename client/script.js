627ee32ea061f14e89fb4fc3e4aaf0f6af8a441d
deoagent <33210588+deoagent@users.noreply.github.com>
2019-04-19 21:35:29 +0800

    Update script.js

--- client/script.js
+++ client/script.js
@@ -4,3 +4,17 @@ const generate = () => {
 }
 window.addEventListener('load', generate);
 document.getElementById('btn__more-quotes').addEventListener('click', generate);
+
+$(document).ready(function(){
+var images=['images/001.jpg',
+            'images/002.jpg',
+            'images/003.jpg',
+            'images/004.jpg',
+            'images/005.jpg',];
+
+var randomNumber = Math.floor(Math.random() * images.length);
+var bgImg = 'url(' + images[randomNumber] + ')';
+
+$('body').css({'background':bgImg, 'background-size':'cover', });
+
+});
