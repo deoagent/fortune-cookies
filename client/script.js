1114e8350933b187caa67a243d7566eb830c5139
deoagent <33210588+deoagent@users.noreply.github.com>
2019-04-19 21:18:56 +0800

    Update script.js
    Add random background images

--- client/script.js
+++ client/script.js
@@ -4,3 +4,21 @@ const generate = () => {
 }
 window.addEventListener('load', generate);
 document.getElementById('btn__more-quotes').addEventListener('click', generate);
+
+// control the auto refresh rate -> 1000 = 1 second
+var refreshRate = 10000; // 10 seconds
+
+
+// Automatically reload the browser
+setInterval(function() {
+   window.location.reload();
+}, refreshRate);
+
+// Hide the images on load
+$('#chicago').hide();
+
+// Get a random image from Unsplash and fade it in on load
+$('#chicago').css({
+  // serves a random image from this collection on Unsplash.com
+  'background-image' : 'url(https://source.unsplash.com/collection/495468/1600x900)'
+}).fadeIn(1500);
